47fcae9243ed8b6492407a955203fc144b3d9b94
sandy <markkratos5252@gmail.com>
2025-04-30 20:05:07 +0530

    final commit

--- hello.sh
+++ hello.sh
@@ -1,3 +1,11 @@
 hello
 THis is the modified hello.sh file
-This file is now modified 2rd time
+This file is now modified 2rd times
+echo "hello"
+echo "THis is the modified hello.sh file"
+
+
+
+echo "This file is now modified 2rd" 
+
+
--- test.py
+++ test.py
@@ -1,2 +1,6 @@
+
 this is the python file
 this is the first modification
+print ("This is line 1")
+print ("THis is line 2")
+
